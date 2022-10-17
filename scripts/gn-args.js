0d8865007549a3988a797d8ff555ba6e4b071fb0
Jimmy Moon <ragingwind@gmail.com>
2022-10-17 20:35:34 +0900

    fix: update gn args

--- scripts/gn-args.js
+++ scripts/gn-args.js
@@ -1,7 +1,9 @@
 const gn_args = {
   // build configs
-  is_official_build: true,
-  is_debug: false,
+  is_official_build: false,
+  is_debug: true,
+  // shared library
+  is_component_build: false,
   // target_cpu: "wasm",, "arm", "arm64", "armeabi-v7a", "arm64-v8a", "x64", "x86", "x86_64"
   // cc: "emcc",
   // cxx: "emcc",
@@ -21,27 +23,29 @@ const gn_args = {
   skia_enable_fontmgr_custom_empty: false,
   skia_use_system_zlib: false,
   // text shaping, https://github.com/harfbuzz/harfbuzz
-  skia_use_harfbuzz: false,
+  skia_use_harfbuzz: true,
+  skia_use_system_harfbuzz: false,
   skia_pdf_subset_harfbuzz: false,
   // unicode, https://icu.unicode.org/
   skia_use_icu: true,
+  skia_use_system_icu: false,
   // skia_use_system_icu: false,
   // xml, https://libexpat.github.io/
-  skia_use_expat: false,
+  skia_use_expat: true,
   // font rendering, https://github.com/rillig/sfntly
   skia_use_sfntly: false,
   // font rendering, http://freetype.org/
   skia_use_freetype: true,
-  skia_use_system_freetype2: false,
   skia_use_freetype_woff2: true,
+  skia_use_system_freetype2: false,
   // graphic library, ANGEL(vulkan, metal, gl), codec configs
-  skia_use_gl: false,
+  skia_use_gl: true,
   skia_use_libjpeg_turbo_decode: false,
   skia_use_libjpeg_turbo_encode: false,
   skia_use_libwebp_decode: false,
   skia_use_libwebp_encode: false,
-  skia_use_libpng_decode: true,
-  skia_use_libpng_encode: false,
+  skia_use_libpng_decode: false,
+  skia_use_libpng_encode: true,
   skia_use_system_libpng: false,
   skia_use_libgifcodec: false,
   skia_use_libheif: false,
